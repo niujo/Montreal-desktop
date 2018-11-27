758d087d19269e571d2d4db1cce4e5a458811119
niujo <cla.cardenasd@gmail.com>
2018-11-27 18:12:07 -0300

    terminandoCEL

--- EscritorioMontreal/RegCel.xaml.cs
+++ EscritorioMontreal/RegCel.xaml.cs
@@ -55,18 +55,54 @@ namespace EscritorioMontreal
         {
             if (txtPsw.Text.Equals(txtRepPsw.Text))
             {
+                /*
+                 * declarando
+                 */
                 Usuario usuario = new Usuario();
                 CEL cel = new CEL();
                 Direccion dir = new Direccion();
                 Contacto cont = new Contacto();
                 Persona per = new Persona();
                 Rol rol = new Rol();
+                /*
+                 * variables bool para  saber si existen en la BD, ya que son unique en la BD
+                 */
+                bool usr = usuario.usuarioExists(txtUsuario.Text);
+                bool rut = per.rutExists(txtRut.Text);
+                /*
+                 * 
+                 */
+                if (usr && rut)
+                {                    
+                    List<object> lstRoles = rol.getRoles();
+                    if (lstRoles == null)
+                    {
+                        lstRoles = new List<object>();
+                    }
+                    for (int i = 0; i < lstRoles.Count; i++)
+                    {
+                        rol = (Rol)lstRoles[i];
+                        if (rol.desc_rol.Equals("CEL"))
+                        {
+                            break;
+                        }
+                    }
+                    usuario.id_rol = rol.id_rol;
+                    usuario.usuario = txtUsuario.Text;
+                    usuario.contrasena = txtPsw.Text;
 
-                usuario.usuario = txtUsuario.Text;
-                usuario.contrasena = txtPsw.Text;
+                    usuario = usuario.guardarUsuario(usuario);
+                    if (usuario != null)
+                    {
+                        dir.id_ciudad = (int)cbCiudad.SelectedItem;
+                        dir.calle = txtCalle.Text;
+                        dir.numeracion = txtNumeracion.Text;
+                        dir.departamento = txtDepartamento.Text;
+
+                    }
+                }
             }
             
-
         }
     }
 }
